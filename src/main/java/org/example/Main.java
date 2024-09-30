00943eb89e972ac4e7dccc1b5ffaa61278cc3bda
ozgur-catal <168709108+ozgur-catal@users.noreply.github.com>
2024-09-30 14:40:09 +0300

    S15G1 Projesi Bitti

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import org.example.mobile.Contact;
+import org.example.mobile.MobilePhone;
+import org.example.models.Grocery;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+
+
+//        List<Contact> contacts = new ArrayList<>();
+//        contacts.add(new Contact("John Doe", "123123123"));
+//        contacts.add(new Contact("Baby Doe", "125125125"));
+//        contacts.add(new Contact("Jane Doe", "124124124"));
+//        mobilePhone = new MobilePhone("11111111", contacts);
+//        Contact contact = new Contact("Test", "12345678");
+//        mobilePhone.removeContact(contact);
+//        System.out.println(mobilePhone.findContact(contact));
+//        System.out.println(mobilePhone.getMyContacts().size());
 
     }
 }
---
+++ src/main/java/org/example/models/Grocery.java
@@ -0,0 +1,66 @@
+package org.example.models;
+import java.util.*;
+import java.util.Scanner;
+
+public class Grocery {
+
+    public static List<String> groceryList = new ArrayList<>();
+
+    public static void startGrocery() {
+        Scanner input = new Scanner(System.in);
+        int number;
+        Scanner items = new Scanner(System.in);
+        while (true) {
+            System.out.println("Lütfen bir değer giriniz (0: Uygulamayı durdur, 1 veya 2): ");
+            number = input.nextInt();
+            switch (number) {
+                case 0:
+                    System.out.println("Uygulama durduruluyor...");
+                    input.close ();
+                    return;
+
+                case 1:
+                    System.out.println("Eklenmesini istediğiniz elemanları giriniz.");
+                    String item = items.nextLine().toLowerCase();
+                    addItems(item);
+                    break;
+
+                case 2:
+                    System.out.println("Çıkarılmasını istediğiniz elemanları giriniz.");
+                    String remove = items.nextLine().toLowerCase();
+                    removeItems(remove);
+                    break;
+
+                default:
+                    System.out.println("Geçersiz giriş! Lütfen 0, 1 veya 2 giriniz.");
+            }
+        }
+    }
+
+    public static void addItems(String input){
+        String[] its = input.split(",");
+        for (String inp: its){
+            if (!checkItemIsIsList(inp.trim())) {
+                groceryList.add(inp.trim());
+            }
+        }
+        printSorted();
+    }
+
+    public static void removeItems(String input){
+        String[] its = input.split(",");
+        for (String inp: its) {
+            if(checkItemIsIsList(inp.trim())) {
+                groceryList.remove(inp.trim());
+            }
+        }
+        printSorted();
+    }
+    public static boolean checkItemIsIsList(String product) {
+        return groceryList.contains(product);
+    }
+    public static void printSorted(){
+        Collections.sort(groceryList);
+        System.out.println(groceryList);
+    }
+}
